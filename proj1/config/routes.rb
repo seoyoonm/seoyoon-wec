d04b589182011c0ba795fc4a5dd1d45f5fa31b72
sychoi <alicia.sychoi@gmail.com>
2014-10-05 18:48:58 +0900

    Define the login route

--- proj1/config/routes.rb
+++ proj1/config/routes.rb
@@ -1,5 +1,6 @@
 Proj1::Application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
+  get 'home/login' => 'home#login'
+	# The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
