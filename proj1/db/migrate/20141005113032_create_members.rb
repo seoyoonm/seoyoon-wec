18cedacfee221750f83559f17a1e370de4568a2c
sychoi <alicia.sychoi@gmail.com>
2014-10-06 15:47:12 +0900

    Add a attribute named count in the table Member

--- proj1/db/migrate/20141005113032_create_members.rb
+++ proj1/db/migrate/20141005113032_create_members.rb
@@ -1,8 +1,9 @@
 class CreateMembers < ActiveRecord::Migration
   def change
     create_table :members do |t|
-			t.string     :username
-			t.string     :password
+			t.string    :username
+			t.string    :password
+			t.int       :count
 
       t.timestamps
     end
