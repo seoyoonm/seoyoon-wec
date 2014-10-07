3d58cb768e06d1fe6c00ae0cec8da46b7e9a4cbb
sychoi <alicia.sychoi@gmail.com>
2014-10-07 20:28:39 +0900

    Add a signup action in the home controller

--- proj1/app/controllers/home_controller.rb
+++ proj1/app/controllers/home_controller.rb
@@ -5,4 +5,10 @@ class HomeController < ApplicationController
 		@username = params[:username]
 		@passwd = params[:passwd]
 	end
+	def signup
+		m = Member.new
+		m.username = params[:username]
+		m.password = params[:passwd]
+		m.save
+	end
 end
