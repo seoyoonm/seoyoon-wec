29105c9cbc6a3e7710f110740b84854c4a040178
sychoi <alicia.sychoi@gmail.com>
2014-10-09 21:30:08 +0900

    make new rails project named Assignment

---
+++ Assignment1/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Assignment1::Application.config.secret_key_base = '6db6a05a761374febc8b5388688c80b38dd9d21ba94669c43241b0616bf3aaaaf7b9207f2cf8af8f9ed59b4f682f5b12f4819ec206da7ff47ce9364c104b398f'
