45ebf234ebb55f47f5b15d0f653a3497ab7a6170
sychoi <alicia.sychoi@gmail.com>
2014-10-09 21:57:57 +0900

    Add UsersModel's attributes and make rules to username and password

--- Assignment1/app/models/users_model.rb
+++ Assignment1/app/models/users_model.rb
@@ -1,2 +1,5 @@
 class UsersModel < ActiveRecord::Base
+	validates :user_name, :length => { :in => 5..20, :wrong_length => "The user name should be 5~20 characters long. Please try again." }
+	validates :password, :length => { :in => 8..20, :wrong_length => "The password should be 8~20 characters long. Please try again." }
+	validates :user_name, :uniqueness => { :message => "The user name already exists. Please try again." }
 end
--- Assignment1/db/migrate/20141009123848_create_users_models.rb
+++ Assignment1/db/migrate/20141009123848_create_users_models.rb
@@ -1,6 +1,9 @@
 class CreateUsersModels < ActiveRecord::Migration
   def change
     create_table :users_models do |t|
+			t.string :user_name
+			t.string :password
+			t.integer :login_count
 
       t.timestamps
     end
